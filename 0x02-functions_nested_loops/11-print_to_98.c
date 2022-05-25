de5ccf361e32099741900659decaa99e08832697
root <nick.french0307@gmail.com>
2022-05-25 13:37:09 -0700

    sweet

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n )
 	{
 		while (n < 98)
 		{
-			printf(%d, ", n)
+			printf("%d, ", n);
 			n++;
 		}
 		printf("98\n");
@@ -21,14 +21,14 @@ void print_to_98(int n )
 	{
 		while (n > 98)
 		{
-			printf("%d, ");
+			printf("%d, ", n);
 			n--;
 		}
-		printf("98\n";
+		printf("98\n");
 	}
 	else
 	{
-		printf(98\n");
+		printf("98\n");
 	}
 }
 
