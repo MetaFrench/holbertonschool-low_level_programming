67e6f9bf01be42bd9368a654e0f4934a47729513
root <nick.french0307@gmail.com>
2022-05-25 07:50:25 -0700

    mess

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+*
+ * Return: Always 0 (Success)
+ */
+
+int man(void)
+{
+	int x = '0';
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+		if (x != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
