486aa37811bafb62369ba93cb57b18b4dcf0a7b3
root <nick.french0307@gmail.com>
2022-05-25 09:03:16 -0700

    mess

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Task 7
+ *
+ * @c: Given from input
+ *
+ * Return: Always C
+ */
+int print_last_digit(int c)
+{
+	c = (c % 10);
+		if (c < '0')
+			c = -c;
+			_putchar(c + 48);
+	return (c);
+}
