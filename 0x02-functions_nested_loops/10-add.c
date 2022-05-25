36ebfbc19a170c6481d1dd924064f5f0634fc1a0
root <nick.french0307@gmail.com>
2022-05-25 12:57:36 -0700

    sweet

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *add - Adds the value of two integers
+ *@x: User inputs first int
+ *@y: User inputs other int
+ *Return: x + y (Success)
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
