5ede2aec85a00d0d0d13cd74e06d7473763437e6
root <nick.french0307@gmail.com>
2022-05-25 10:07:36 -0700

    noice

---
+++ 0x02-functions_nested_loops/9-times_tables.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *times_table
+ *
+ *@n:
+Return:
+ */
+void times_table(void)
+{
+	int num;
+	int mult;
+	int prod;
+		for (num = 0; num <= 9; num ++)
+		{
+			_putchar('0');
+			for (mult = 1; mult <= 9; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				prod = num * multi;
+				if (prod <= 9)
+				{
+					_putchar(' ');
+				}else
+				{
+				_putchar ('0' + (prod / 10));
+				}
+			_putchar('0' + (prod % 10));
+			}
+			_putchar('\n');
+		}
+}
