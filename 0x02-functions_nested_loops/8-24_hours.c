65955927b632b2099bd11d7769a82da47d49c906
root <nick.french0307@gmail.com>
2022-05-25 09:42:11 -0700

    nice

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,8 +8,8 @@ void jack_bauer(void)
 		{
 			for (m = 0; m <= 59; m++)
 			{
-				_putchar('0'+(h / 10));
-				_putchar('0'+(h % 10));
+				_putchar('0' + (h / 10));
+				_putchar('0' + (h % 10));
 				_putchar(':');
 				_putchar('0' + (m / 10));
 				_putchar('0' + (m % 10));
