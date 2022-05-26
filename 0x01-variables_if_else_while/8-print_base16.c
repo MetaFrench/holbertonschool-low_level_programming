c17f11b3248aa5f39f89ba36aa6fbd077b9d7122
root <nick.french0307@gmail.com>
2022-05-25 19:24:30 -0700

    mess

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	{
+	int g;
+
+	for (g = 48; g <= 57; g++)
+		putchar(n);
+	for (g = 97; g <= 102; g++)
+		putchar(g);
+	putchar(10);
+	return (0);
+}
