11dacfca0a405331cfbd86cf11f31ef0b212751b
root <nick.french0307@gmail.com>
2022-05-31 14:40:54 -0700

    bet

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0 through 9
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for ( n = 48; n <= 57; n++)
+	{
+		_putchar(n);
+	}
+	_putchar(10);
+}
