e90520bb67e73956da1552877073dd110c798a61
root <nick.french0307@gmail.com>
2022-05-31 16:11:01 -0700

    mess

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ * print_most_numbers - prints the numbers 0 through 9 and not 2 and 4
+ *
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		if (n != 50 && n != 52)
+		_putchar(n);
+	}
+	_putchar(10);
+}
