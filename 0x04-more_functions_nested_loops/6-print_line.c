6852e8d4966654bedc82b401ad809ada197d0383
root <nick.french0307@gmail.com>
2022-05-31 18:40:26 -0700

    mess

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - prints n underscore characters
+ *
+ * @n: integer
+ *
+ * Return: Always 0 
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		while (n > 0)
+		{
+			_putchar(95);
+			n--;
+		}
+	}
+	_putchar(10);
+}
