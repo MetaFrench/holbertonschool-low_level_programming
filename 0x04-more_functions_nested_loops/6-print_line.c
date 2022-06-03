d7731dde0090721db04aa398d30fbbebcbd8b282
root <nick.french0307@gmail.com>
2022-06-03 12:19:08 -0700

    mess

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers 0 through 9, followed by a new line
- *
+ * print_line - prints the numbers 0 through 9, followed by a new line
+ * @n: int 
  * Return: Always 0
  */
-void print_numbers(void)
+void print_line(int i)
 {
 	int n;
 
 	for (n = 48; n <= 57; n++)
 	{
-		_putchar(n);
+		_putchar(i);
 	}
 	_putchar(10);
 }
