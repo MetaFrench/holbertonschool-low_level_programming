df23b9369278ea85c39d3ca0a133d07b1012d283
root <nick.french0307@gmail.com>
2022-06-02 20:15:38 -0700

    mess

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * puts_half - prints the back half of string
+  *
+  * Return: void (success)
+  */
+
+void puts_half(char *str)
+{
+	int v = 0;
+
+	while (str[v] != '\n' && str[v] != '\0')
+	{
+		v++;
+	}		
+	for (
+
+
+
+	}
+	_putchar('\n');
+}	
