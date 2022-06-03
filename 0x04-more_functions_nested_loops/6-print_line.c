a0b69606304dfc6807e53e219563e308d6f8cfdc
root <nick.french0307@gmail.com>
2022-06-03 11:36:12 -0700

    mess

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * print_line - print "_"  * n times.
+ * print_line - prints a line composed of n underscore characters
  *
- * @n: number of times to print "_".
+ * @n: integer, number of times underscore should be repeated
+ *
+ * Return: Always 0
  */
-
 void print_line(int n)
 {
-	int i;
-
-	i = 0;
-	while (n > 0 && i < n)
+	if (n > 0)
 	{
-		_putchar(95);
-
-		i++;
+		while (n > 0)
+		{
+			_putchar(95);
+			n--;
+		}
 	}
-	_putchar('\n');
+	_putchar(10);
 }
