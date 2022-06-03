b1dd5d0f0d7a4eb7d4ec96af2bd61b90e8047b9e
root <nick.french0307@gmail.com>
2022-06-03 12:08:00 -0700

    mess

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,21 +1,17 @@
 #include "main.h"
 
 /**
- * print_line - prints a line composed of n underscore characters
- *
- * @n: integer, number of times underscore should be repeated
+ * print_numbers - prints the numbers 0 through 9, followed by a new line
  *
  * Return: Always 0
  */
-void print_line(int n)
+void print_numbers(void)
 {
-	if (n > 0)
+	int n;
+
+	for (n = 48; n <= 57; n++)
 	{
-		while (n > 0)
-		{
-			_putchar(95);
-			n--;
-		}
+		_putchar(n);
 	}
 	_putchar(10);
 }
